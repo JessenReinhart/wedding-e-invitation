817c4a08ad92f3d444de957fa606f8845e4632ae
JessenReinhart <jessen_1206@yahoo.com>
2025-07-01 22:17:44 +0700

    chore: remove unused import of AnimatedLeaves in Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import AnimatedLeaves from './AnimatedLeaves';
 
 interface HeroProps {
   brideName: string;
